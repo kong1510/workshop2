e8d8973a9068d4edbf2655e7d8da273306a76d53
kong1510 <pakakida_kong@hotmail.com>
2021-01-10 14:35:19 +0700

    add variable name

---
+++ variable/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้
+myvar = "kong"
+my_var = "kong"
+_my_var = "kong"
+myVar = "kong"
+MYVAR = "kong"
+myvar2 = "kong"
+
+#ไม่สามารถประกาศได้
+2myvar = "kong"
+my-var = "kong"
+My var = "kong"
