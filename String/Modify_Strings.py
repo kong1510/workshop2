b34e846faabce207075805aadac844173ec9027d
kong1510 <pakakida_kong@hotmail.com>
2021-01-16 14:34:29 +0700

    add modify string

---
+++ String/Modify_Strings.py
@@ -0,0 +1,8 @@
+string = "Hello, World !"
+
+print(string.upper())  # output : " Hello, World!"
+print(string.lower())  # output : " hellow, world!"
+print(string.strip())  # output : "Hello, World!"
+print(string.replace("H", "J"))  # output : Jello, World!
+print(string.split(","))  # output : ['Hello','World!']
+print(len(string))  # 15
