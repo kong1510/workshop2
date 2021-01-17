e51bb3c7a02a3aa2d69a69c3371516844b222c2e
kong1510 <pakakida_kong@hotmail.com>
2021-01-17 16:01:08 +0700

    ex set

---
+++ Sets/Access Set items.py
@@ -0,0 +1,10 @@
+# Ex1
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+    print(x)
+
+# Ex2
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits
+    print("Yes, apple is a fruit!")
